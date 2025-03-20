afa4b14dad170ac10e07b8cefb139d0c59710178
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-03-20 11:54:26 +0530

    [komal]Adding UC3

--- Employee.js
+++ Employee.js
@@ -37,3 +37,20 @@ switch (empCheck) {
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
 
+// UC 3
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
